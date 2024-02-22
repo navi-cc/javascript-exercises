419aa799e89c6e7b25632d17344cbdb0901fa5f2
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-02-22 12:20:44 +0800

    Complete second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+
+    let str_concat = "";
+
+
+    if (n < 0) {
+        return "ERROR";
+    } else {
+
+        for (let i = 0; i < n; i++) {
+            str_concat += str;
+        }
+
+    } 
+
+    return str_concat;
 
 };
 
