f2051778031ea6af340d6f48552f09a147cf518f
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-02-25 17:08:12 +0800

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,40 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...array2) {
+
+    const mergedArray = [...array1, ...array2].sort();
+
+    const getDuplicatedItems = mergedArray.filter((item, index) => mergedArray.indexOf(item) !== index);
+    
+    const getUniqueItems = mergedArray.filter((item) => {
+
+        for (let i = 0; i < getDuplicatedItems.length; i++) {
+            if (getDuplicatedItems[i] == item) return false;
+        }
+
+        return true;
+
+    });
+
+    let arr2 = [...array2];
+
+    const getNewArray = getUniqueItems.filter((item) => {
+
+        for (let i = 0; i < arr2.length; i++) {
+            if (arr2[i] === item) return false;
+        }
+
+        return true;
+
+    }); 
+
+    
+    return getNewArray;
 
 };
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
