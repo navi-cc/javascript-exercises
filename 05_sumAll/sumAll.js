079896c75fe9b5cf727567fa2f8fd234ddd10a69
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-02-26 16:25:18 +0800

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+    
+    if ((num1 <= -1 || num2 <= -1 ) || 
+        (typeof num1 !== "number" || typeof num2 !== "number")) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;   
+        } 
+    }
+
+    for (let i = num1; i <= num2; i++) {
+            sum += i;   
+    } 
+
+    return sum;
+        
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
