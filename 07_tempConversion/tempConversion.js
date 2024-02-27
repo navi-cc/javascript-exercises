bb7c9611e44c55550ee43f5c89f25b37a86c2461
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-02-27 12:54:57 +0800

    Complete seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+
+  number = (number - 32) / (9/5);
+  
+  const round = (number, precise) => {
+
+      let multiplier = Math.pow(10, precise || 0);
+
+      return Math.round(number * multiplier) / multiplier;
+
+  }
+
+  return round(number, 1);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+
+  number = ((9/5) * number) + 32;
+  
+  const round = (number, precise) => {
+
+    let multiplier = Math.pow(10, precise || 0);
+
+    return Math.round(number * multiplier) / multiplier;
+
+  }
+
+  return round(number, 1);
+
 };
 
 // Do not edit below this line
