a616ed63ab0eef0b8d44f75df72fbc2da6a4098e
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-03-22 20:32:14 +0800

    Complete eigth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1 , num2) {
+    return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, num) => total += num , 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((total, num) => total *= num , 1);
+};
+
+const power = function(num, exponent) {
+	return num ** exponent;
+};
+
+const factorial = function(num) {
+
+  let factorial = 1;
+
+  if (num === 0 || num === 1) return factorial;
+
+
+  for (let i = 1; i <= num; i++) {
+    factorial *= i
+  }
+
+  return factorial;
+
+
+  
+
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
