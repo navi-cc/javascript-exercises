94ec65909d2b539ee181131ba896ae51093a8f62
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-03-24 19:18:41 +0800

    Complete eleventh exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+    return book.reduce((title, item, index) => {
+        title[index] = item.title;
+        return title;
+    }, []);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
