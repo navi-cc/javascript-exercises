451111c73478af95277142ca49044c71cfccd40d
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-03-24 19:07:52 +0800

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    let a;
 
+    if (typeof n !== "number") n = parseInt(n); 
+    if (n < 0) return 'OOPS';
+    if (n === 0) return 0;
+
+    let arr = [1];
+
+    let previousIndex = 0;
+    for (let i = 0; i < n; i++) {
+
+        if (i === 0) {
+        a = arr[i] + i;
+        arr.push(a);
+        continue;
+        }  
+        
+        a = arr[i] + arr[previousIndex]
+        previousIndex++
+        arr.push(a);
+    }
+
+    return arr[arr.length - 2];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
