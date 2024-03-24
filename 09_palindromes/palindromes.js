907a20a7a4561a47d8e8f4293fdb4942df734517
navi-cc <157870465+navi-cc@users.noreply.github.com>
2024-03-24 15:55:07 +0800

    Complete ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let reversedString = string.toLowerCase().split(/\W/gm).join('').split('').reverse().join('');
+    string = string.toLowerCase().split(/\W/gm).join('');
+    return string === reversedString
 };
-
-// Do not edit below this line
 module.exports = palindromes;
